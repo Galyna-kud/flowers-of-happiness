d2067fb78b484e07314fe263a0ddc6bec806a111
Galyna Kud <galynakud121@gmail.com>
2025-12-19 20:45:13 +0200

    changes

---
+++ src /lib /firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjb3Ld-62hDNEHkbasf-5l9XNmAJe1Q3M",
+  authDomain: "myprojectauth-fea1a.firebaseapp.com",
+  projectId: "myprojectauth-fea1a",
+  storageBucket: "myprojectauth-fea1a.firebasestorage.app",
+  messagingSenderId: "1045500546310",
+  appId: "1:1045500546310:web:54ca2e8b9580e0b7226287",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
